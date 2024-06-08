4558408527fd71ddf79332e364f2d8da1de37c6b
Santiago0212004 <sjbs0212@gmail.com>
2024-06-08 00:17:58 -0500

    user from viewmodel to delete

--- app/src/main/java/com/example/unistylejc/screens/CustomerSettingsScreen.kt
+++ app/src/main/java/com/example/unistylejc/screens/CustomerSettingsScreen.kt
@@ -1,11 +1,21 @@
 package com.example.unistylejc.screens
 
-import androidx.compose.runtime.Composable
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -14,47 +24,48 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import coil.compose.rememberAsyncImagePainter
 import com.example.unistylejc.R
 import com.example.unistylejc.domain.model.Customer
-import com.example.unistylejc.repository.UserRepository
-import com.example.unistylejc.repository.UserRepositoryImpl
 import com.example.unistylejc.viewmodel.CustomerProfileViewModel
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
-import com.google.firebase.firestore.firestore
-import com.google.firebase.storage.storage
 
 @Composable
 private fun ScreenContent(navController: NavHostController,userState: Customer?) {
     val viewModel: CustomerProfileViewModel = viewModel()
     var showDialogDA by remember { mutableStateOf(false) }
-    var email by remember { mutableStateOf(Firebase.auth.currentUser?.email ?: "") }
     var pass by remember { mutableStateOf("") }
     val errorState by viewModel.errorState.observeAsState()
-    val userRepo: UserRepository = UserRepositoryImpl()
+    val user by viewModel.userState.observeAsState()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -104,11 +115,13 @@ private fun ScreenContent(navController: NavHostController,userState: Customer?)
             onPassChange = { pass = it },
             onConfirm = {
                 val id = Firebase.auth.currentUser!!.uid
-                viewModel.deleteAccount(email, pass,id,
-                    onSuccess = {
-                        navController.navigate("login")
-                    }
-                )
+                user?.let {
+                    viewModel.deleteAccount(it.email, pass, it.id,
+                        onSuccess = {
+                            navController.navigate("login")
+                        }
+                    )
+                }
                 showDialogDA = false
             },
             onDismiss = {
@@ -141,14 +154,14 @@ private fun ProfileSection(navController: NavHostController,userState: Customer?
                 modifier = Modifier
                     .size(124.dp)
                     .clip(CircleShape)
-                    .clickable {  navController.navigate("customer/settings/updateProfile") },
+                    .clickable { navController.navigate("customer/settings/updateProfile") },
                 contentScale = ContentScale.Crop
             )
             Spacer(modifier = Modifier.width(16.dp))
             Box(
                 modifier = Modifier
                     .size(width = 220.dp, height = 124.dp)
-                    .shadow(3.dp,  shape = RoundedCornerShape(16.dp))
+                    .shadow(3.dp, shape = RoundedCornerShape(16.dp))
                     .background(color = Color.White, shape = RoundedCornerShape(16.dp))
                     .padding(16.dp)
                 ,) {
